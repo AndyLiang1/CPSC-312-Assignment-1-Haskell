8f36f55ac52adca0b399f737774ca2d8b6549dd2
lluey <lluey@students.ubc.ca>
2023-02-23 22:31:55 -0800

    Added implementation with random words

---
+++ WordleLL.hs
@@ -0,0 +1,118 @@
+import System.Random
+import Data.Char
+
+data State = State [String] Int
+
+data Result = EndOfGame Double State
+    | ContinueGame State
+
+type Game = Action -> Action -> State -> Result
+
+data Action = Action String             -- a move for a player is a String
+    deriving Eq
+
+wordle :: Game
+wordle move answer (State history attempts)
+    | move == answer            = EndOfGame 1 wordle_start
+    | attempts == 1             = EndOfGame 0 wordle_start
+    | otherwise                 = 
+        do 
+        let response = filter_word move answer
+        let Action guess = move
+        ContinueGame (State (history++[show move ++ "|" ++ response]) (attempts-1))
+
+wordle_start = State [] 6
+    
+-- randomElement :: [a] -> IO a
+randomElement list = do
+  gen <- getStdGen
+  let (i, g) = randomR (0, length list - 1) gen
+  return $ list !! i
+
+
+-- show and read actions just as the String
+instance Show Action where
+    show (Action i) = show i
+
+
+type WordleState = (Int, Int)         -- wins, losses
+
+start :: Game -> State -> WordleState -> IO WordleState
+start game start_state ws = 
+    let (wins, losses) = ws in
+    do
+        putStrLn("Stats: "++ show wins++ " wins " ++show losses++" losses")
+        putStrLn("Would you like to exit?")
+        line <- getLine
+        if line == "yes"
+            then
+                do
+                    putStrLn "Thanks for playing!" 
+                    return ws
+            else
+                do
+                    answer <- randomWord
+                    play game (Action answer) (ContinueGame start_state) ws
+
+play :: Game -> Action -> Result -> WordleState -> IO WordleState
+play game answer (ContinueGame state) ws =
+    do
+        putStrLn("--------------------------")
+        displayInfo state
+
+        putStrLn("Enter your guess")
+        line <- getLine
+        
+        if length line == 5
+            then
+                if (all isLower line)
+                    then
+                    do
+                        play game answer (game (Action line) answer state) ws
+                    else
+                        do
+                            putStrLn("Illegal move " ++ line ++ ". Please input all lowercase.")
+                            play game answer (ContinueGame state) ws
+            else
+                do
+                    putStrLn("Illegal move " ++ line ++ ". Please input a 5 letter guess.")
+                    play game answer (ContinueGame state) ws
+
+play game answer (EndOfGame val start_state) ws =
+    do
+        newws <- update_wordle_state val answer ws
+        start game start_state newws
+
+randomWord = (word_bank !!) <$> randomRIO (0, length word_bank - 1)
+
+filter_word (Action guess) (Action answer) = filter_guess guess (filter_answer guess answer) 0
+
+filter_answer :: String -> String -> String
+filter_answer [] answer = []
+filter_answer (x:xs) (h:t)
+    | x == h = 'G':(filter_answer xs t)
+    | otherwise = h:(filter_answer xs t)
+
+filter_guess [] answer n = []
+filter_guess (x:xs) answer n
+    | answer!!n == 'G' = 'G':(filter_guess xs answer (n+1))
+    | x `elem` answer = 'Y':(filter_guess xs answer (n+1))
+    | otherwise = 'X':(filter_guess xs answer (n+1))
+
+displayInfo (State history attempts) = 
+    do
+        putStrLn ("You have " ++ show attempts ++ " attempts left.")
+        mapM_ putStrLn history
+    
+update_wordle_state val (Action answer) (wins, losses)
+    | val == 1 = do
+        putStrLn "You Won"
+        return (wins+1, losses)
+    | otherwise = do
+        putStrLn("You lost!")
+        putStrLn("The correct answer: "++answer)
+        return (wins, losses+1)
+
+begin = start wordle wordle_start (0, 0)
+
+word_bank = ["which", "their", "would", "there", "could", "other", "about", "great", "these", "after", "first", "never", "where", "those", "shall", "being", "might", "every", "think", "under", "found", "still", "while", "again", "place", "young", "years", "three", "right", "house", "whole", "world", "thing", "night", "going", "heard", "heart", "among", "asked", "small", "woman", "whose", "quite", "words", "given", "taken", "hands", "until", "since", "light", "began", "large", "water", "works", "often", "stood", "power", "money", "order", "means", "round", "voice", "white", "point", "state", "above", "death", "least", "known", "along", "leave", "alone", "women", "times", "speak", "forth", "terms", "cried", "child", "human", "short", "cause", "seems", "bring", "doubt", "black", "sense", "close", "truth", "ought", "party", "ready", "force", "early", "earth", "sight", "spoke", "story", "later", "added", "stand", "nicht", "again", "miles", "comes", "table", "hours", "river", "happy", "clear", "sound", "makes", "blood", "comme", "doing", "avait", "tried", "front", "quill", "peace", "lived", "horse", "wrote", "paper", "cette", "chief", "books", "visit", "heavy", "knows", "loved", "carry", "plain", "sweet", "write", "trees", "below", "wrong", "reach", "noble", "parts", "agree", "moved", "enemy", "worth", "green", "third", "mouth", "sleep", "fresh", "faith", "there", "smile", "usual", "bound", "quiet", "etext", "court", "youth", "piece", "meant", "world", "seven", "tears", "value", "broke", "fight", "stone", "begin", "learn", "lines", "grand", "takes", "month", "girls", "gives", "eight", "scene", "lives", "drawn", "fifty", "field", "chair", "named", "allow", "music", "fixed", "study", "spent", "trust", "break", "equal", "threw", "watch", "looks", "built", "using", "spite", "moral", "walls", "touch", "steps", "offer", "house", "dress", "lying", "grave", "legal", "lower", "other", "cases", "night", "shown", "names", "board", "faire", "glass", "share", "forms", "class", "start", "shook", "train", "enter", "prove", "floor", "worse", "sorry", "pride", "place", "marry", "crowd", "shore", "drink", "judge"]
